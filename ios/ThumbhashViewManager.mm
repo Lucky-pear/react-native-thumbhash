c37aa68c1099929f7be81f933830b766b1300c58
camou <jbyun94@naver.com>
2024-07-22 20:46:09 +0900

    feat: add decode async

--- ios/ThumbhashViewManager.mm
+++ ios/ThumbhashViewManager.mm
@@ -5,5 +5,10 @@
                                     RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(thumbhash, NSString);
+RCT_EXPORT_VIEW_PROPERTY(decodeAsync, BOOL);
+
+RCT_EXPORT_VIEW_PROPERTY(onLoadStart, RCTDirectEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onLoadEnd, RCTDirectEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onLoadError, RCTDirectEventBlock);
 
 @end
--- src/ThumbhashViewNativeComponent.ts
+++ src/ThumbhashViewNativeComponent.ts
@@ -1,8 +1,16 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { ViewProps } from 'react-native';
+import type {
+  DirectEventHandler,
+  WithDefault,
+} from 'react-native/Libraries/Types/CodegenTypes';
 
 interface NativeProps extends ViewProps {
   thumbhash: string;
+  decodeAsync?: WithDefault<boolean, false>;
+  onLoadStart?: DirectEventHandler<null>;
+  onLoadEnd?: DirectEventHandler<null>;
+  onLoadError?: DirectEventHandler<{ message?: string }>;
 }
 
 export default codegenNativeComponent<NativeProps>('ThumbhashView');
